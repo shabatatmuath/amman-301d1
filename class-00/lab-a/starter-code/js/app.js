c2fae63fce1845b4a9e71fe48c807f2a3fcdbe56
muathshabatat <shabatatmuath@gmail.com>
2019-11-12 09:20:41 +0200

    final lab a

--- class-00/lab-a/starter-code/js/app.js
+++ class-00/lab-a/starter-code/js/app.js
@@ -28,19 +28,18 @@ function makeRandom() {
   return Math.floor(Math.random() * names.length);
 }
 
-function displayPics(){
-  while(viewed.length < 6){
+function displayPics() {
+  while (viewed.length < 6) {
     const rando = makeRandom();
-    while(!viewed.includes(rando)){
+    while (!viewed.includes(rando)) {
       viewed.push(rando);
     }
   }
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the constiable declaration from `const to `const`.
-  // PUT YOUR RESPONSE IN THIS COMMENT
   // because rando is not aglobal var
   console.log(viewed);
 
-  for (let i = 0; i < 3; i++){
+  for (let i = 0; i < 3; i++) {
     const temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
@@ -53,16 +52,16 @@ function handleClick(event) {
     return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
-  if(totalClicks > 24) {
+  if (totalClicks > 24) {
     container.removeEventListener('click', handleClick);
     container.style.display = 'none';
     showList();
     makeChart();
   }
-  for(let i = 0; i < names.length; i++){
-    if(event.target.id === allProducts[i].name) {
+  for (let i = 0; i < names.length; i++) {
+    if (event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(`${event.target.id}  ' has ' ${ allProducts[i].votes } ' votes in ' ${ allProducts[i].views } ' views'`);
+      console.log(`${event.target.id}  ' has ' ${allProducts[i].votes} ' votes in ' ${allProducts[i].views} ' views'`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -71,22 +70,22 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(let i = 0; i < allProducts.length; i++) {
+  for (let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = `${allProducts[i].name } ' has ' ${ allProducts[i].votes } ' votes in ' ${ allProducts[i].views } ' views'`;
+    liEl.textContent = `${allProducts[i].name} ' has ' ${allProducts[i].votes} ' votes in ' ${allProducts[i].views} ' views'`;
     list.appendChild(liEl);
   }
 }
 
-function makeChartData(){
-  allProducts.forEach(function(product){
+function makeChartData() {
+  allProducts.forEach(function (product) {
     labels.push(product.name);
     votes.push(product.votes);
     views.push(product.views);
   });
 }
 
-function makeChart(){
+function makeChart() {
   makeChartData();
   const ctx = document.getElementById('chartypants').getContext('2d');
   new Chart(ctx, { //eslint-disable-line
@@ -118,17 +117,17 @@ function makeChart(){
 
 container.addEventListener('click', handleClick);
 
-document.getElementById('bus').addEventListener('click', function(){
+document.getElementById('bus').addEventListener('click', function () {
   localStorage.removeItem('busmall');
   console.log('Local storage was cleared!');
 });
 
-if(localStorage.busmall){
+if (localStorage.busmall) {
   console.log('Local storage data exists');
   allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
-  for(let i = 0; i < names.length; i++) {
+  for (let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
 }
